96dfdb3245c65b31a53fbe92531d580418c8aef2
cout <cout@e2d4e900-5bc4-4db4-8b5f-7ecdbb188e69>
2008-02-01 18:26:09 +0000

    Append EXEEXT to executable names.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rice/trunk@166 e2d4e900-5bc4-4db4-8b5f-7ecdbb188e69

--- test/test_rice.rb
+++ test/test_rice.rb
@@ -1,15 +1,18 @@
 require 'test/unit'
+require 'rbconfig'
 
 class RiceTest < Test::Unit::TestCase
   # TODO: probably a better way to find this out...
   VERBOSE = ARGV.include?('-v')
 
+  EXEEXT = Config::CONFIG['EXEEXT']
+
   def test_unittest
-    run_external_test('./unittest')
+    run_external_test("./unittest#{EXEEXT}")
   end
 
   def test_vm_unittest
-    run_external_test('./vm_unittest')
+    run_external_test("./vm_unittest#{EXEEXT}")
   end
 
   def run_external_test(executable)
