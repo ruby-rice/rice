cb5a8c0c55cbad97f58c7d870e4e5dbd27cd29fc
Paul Brannan <curlypaul924@gmail.com>
2008-11-27 18:46:43 -0500

    Added documentation for ruby_mark<>.

--- rice/ruby_mark.hpp
+++ rice/ruby_mark.hpp
@@ -1,6 +1,10 @@
 #ifndef ruby_mark__hpp
 #define ruby_mark__hpp
 
+//! Default function to call to mark a data object.
+/*! This function can be specialized for a particular type to override
+ *  the default behavior (which is to not mark any additional objects).
+ */
 template<typename T>
 void ruby_mark(T * obj)
 {
