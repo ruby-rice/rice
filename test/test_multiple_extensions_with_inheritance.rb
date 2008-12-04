02700a4d09c102543659efddaf17935fc0b984e3
Paul Brannan <curlypaul924@gmail.com>
2008-12-04 15:29:49 -0500

    Added new test.

---
+++ test/test_multiple_extensions_with_inheritance.rb
@@ -0,0 +1,18 @@
+$: << File.join(File.dirname(__FILE__), '..', 'sample')
+
+require 'test/unit'
+
+class MultipleExtensionTest < Test::Unit::TestCase
+  def test_multiple_extensions_with_inheritance
+    # Load a library that uses inheritance
+    require 'inheritance/animals'
+
+    # Then load a different library
+    require 'enum/sample_enum'
+
+    # And make sure we can call methods in the base class on a derived
+    # instance
+    dog = Dog.new
+    dog.speak
+  end
+end
