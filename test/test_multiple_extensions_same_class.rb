80d26a14f7c32e694b40ce0a4c69675cbbc4b76d
Paul Brannan <cout@sapphire.(none)>
2009-11-18 09:40:04 -0500

    Changed class name.

--- test/test_multiple_extensions_same_class.rb
+++ test/test_multiple_extensions_same_class.rb
@@ -2,8 +2,8 @@ $: << File.join(File.dirname(__FILE__), 'ext')
 
 require 'test/unit'
 
-class MultipleExtensionTest < Test::Unit::TestCase
-  def test_multiple_extensions_with_inheritance
+class MultipleExtensionsSameClassTest < Test::Unit::TestCase
+  def test_multiple_extensions_same_class
     require 't1/t1'
     require 't2/t2'
 
