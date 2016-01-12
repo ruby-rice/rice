6d72add5909019628d224319be1beb386b8f2b7d
Jason Roelofs <jasongroelofs@gmail.com>
2016-01-12 17:01:46 -0500

    Update documentation to version 2.1.0

--- search/all_12.js
+++ search/all_12.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['tag',['tag',['../structRice_1_1Jump__Tag.html#a76399ed877098537308c2d0a3c40bdaf',1,'Rice::Jump_Tag']]],
   ['test',['test',['../classRice_1_1Object.html#a3771564aa595c8ed994777e5e9757fb1',1,'Rice::Object']]],
-  ['to_5fc_5farray',['to_c_array',['../classRice_1_1Array.html#ae5e4b10474269c646090db386a57a64a',1,'Rice::Array']]],
   ['to_5fid',['to_id',['../classRice_1_1Symbol.html#ac689f037ce1a1013d433122c1ebe2e27',1,'Rice::Symbol']]],
   ['to_5fruby_5f_3c_20data_5fobject_3c_20t_20_3e_20_3e',['to_ruby_&lt; Data_Object&lt; T &gt; &gt;',['../structRice_1_1detail_1_1to__ruby___3_01Data__Object_3_01T_01_4_01_4.html',1,'Rice::detail']]],
   ['to_5fs',['to_s',['../classRice_1_1Object.html#a534c447e5adc81f0f3c553ccf9985c16',1,'Rice::Object']]],
--- search/functions_f.js
+++ search/functions_f.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['pop',['pop',['../classRice_1_1Array.html#acb2d159045d6091febbc11c4a9e0c9de',1,'Rice::Array']]],
+  ['protect',['protect',['../protect_8hpp.html#a0c7631300bafac97e65355a5ef017228',1,'Rice']]],
   ['proxy',['Proxy',['../classRice_1_1Array_1_1Proxy.html#a9cc9fc5ec299fdd277b853cbe628598c',1,'Rice::Array::Proxy::Proxy()'],['../classRice_1_1Hash_1_1Proxy.html#a86222b7b443f23154092ccfcbd976e4a',1,'Rice::Hash::Proxy::Proxy()']]],
   ['push',['push',['../classRice_1_1Array.html#ac8225c89553bb7e61277921e06858184',1,'Rice::Array']]]
 ];
