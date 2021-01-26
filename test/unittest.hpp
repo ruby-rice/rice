141a7f20466e1b71f034a83814cc4f0cfd7cf44c
Charlie Savage <cfis@savagexi.com>
2021-01-26 00:48:03 -0800

    Remove previous addition of adding ruby support directly to unittest. Better to use Object==

--- test/unittest.hpp
+++ test/unittest.hpp
@@ -15,8 +15,6 @@
 #include <sstream>
 #include <iostream>
 
-#include "ruby.h"
-
 class Failure
 {
 public:
@@ -206,11 +204,6 @@ inline bool is_equal(char * lhs, char const * rhs)
 inline bool is_equal(char const * lhs, char * rhs)
 {
   return std::string(lhs) == std::string(rhs);
-}
-
-inline bool is_equal(VALUE lhs, VALUE rhs)
-{
-  return rb_equal(lhs, rhs);
 }
 
 template<typename T, typename U>
