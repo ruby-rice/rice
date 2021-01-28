6e1c982248759159201b976bf32668d5fb25506f
Charlie Savage <cfis@savagexi.com>
2021-01-27 23:06:20 -0800

    Remove patches for older versions of Ruby and move undefs up.

--- rice/detail/ruby.hpp
+++ rice/detail/ruby.hpp
@@ -16,6 +16,17 @@
 
 #include <ruby.h>
 
+// ruby.h has a few defines that conflict with Visual Studio's STL
+#if defined(_MSC_VER)
+#undef write
+#undef read
+#undef bind
+#endif
+
+// And some c library conflicts
+#undef snprintf
+#undef vsnprintf
+
 //! A function that takes a VALUE as a parameter and returns a VALUE.
 // TODO: Casting from a C++ function to an extern "C" function won't
 // work on all platforms.  I'm not sure what to do about this.
@@ -26,41 +37,6 @@ extern "C" typedef VALUE (*RUBY_VALUE_FUNC)(VALUE);
 # undef RUBY_METHOD_FUNC
   extern "C" typedef VALUE (*RUBY_METHOD_FUNC)(ANYARGS);
 #endif
-
-#ifndef RSTRING_LEN
-#define RSTRING_LEN(str) RSTRING(str)->len
-#endif
-
-#ifndef RSTRING_PTR
-#define RSTRING_PTR(str) RSTRING(str)->ptr
-#endif
-
-#ifndef RARRAY_LEN
-#define RARRAY_LEN(arr) RARRAY(arr)->len
-#endif
-
-#ifndef RARRAY_PTR
-#define RARRAY_PTR(arr) RARRAY(arr)->ptr
-#endif
-
-#ifndef RHASH_TBL
-#define RHASH_TBL(hsh) RHASH(hsh)->tbl
-#endif
-
-#ifndef RCLASS_M_TBL
-#define RCLASS_M_TBL(c) RCLASS(c)->m_tbl
-#endif
-
-// ruby.h has a few defines that conflict with Visual Studio's STL
-#if defined(_MSC_VER)
-  #undef write
-  #undef read
-  #undef bind
-#endif
-
-// And some c library conflicts
-#undef snprintf
-#undef vsnprintf
 
 #endif // Rice__detail__ruby__hpp_
 
