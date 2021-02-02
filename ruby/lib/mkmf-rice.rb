0d40fe4f98b7986f8c5177477b0274ee2968653b
Charlie Savage <cfis@savagexi.com>
2021-02-01 21:20:23 -0800

    Backpatch mkmf C++ support that was added in Ruby 2.7

--- ruby/lib/mkmf-rice.rb
+++ ruby/lib/mkmf-rice.rb
@@ -1,5 +1,56 @@
 require 'mkmf'
 
+# If we are on versions of Ruby before 2.7 then we need to copy in the experimental C++ support
+# added in Ruby 2.7
+unless MakeMakefile.method('[]')
+  MakeMakefile.module_eval do
+    @lang = Hash.new(self)
+
+    def self.[](name)
+      @lang.fetch(name)
+    end
+
+    def self.[]=(name, mod)
+      @lang[name] = mod
+    end
+
+    MakeMakefile["C++"] = Module.new do
+      include MakeMakefile
+      extend self
+
+      CONFTEST_CXX = "#{CONFTEST}.#{config_string('CXX_EXT') || CXX_EXT[0]}"
+
+      TRY_LINK_CXX = config_string('TRY_LINK_CXX') ||
+        ((cmd = TRY_LINK.gsub(/\$\(C(?:C|(FLAGS))\)/, '$(CXX\1)')) != TRY_LINK && cmd) ||
+        "$(CXX) #{OUTFLAG}#{CONFTEST}#{$EXEEXT} $(INCFLAGS) $(CPPFLAGS) " \
+                     "$(CXXFLAGS) $(src) $(LIBPATH) $(LDFLAGS) $(ARCH_FLAG) $(LOCAL_LIBS) $(LIBS)"
+
+      def have_devel?
+        unless defined? @have_devel
+          @have_devel = true
+          @have_devel = try_link(MAIN_DOES_NOTHING)
+        end
+        @have_devel
+      end
+
+      def conftest_source
+        CONFTEST_CXX
+      end
+
+      def cc_command(opt="")
+        conf = cc_config(opt)
+        RbConfig::expand("$(CXX) #$INCFLAGS #$CPPFLAGS #$CXXFLAGS #$ARCH_FLAG #{opt} -c #{CONFTEST_CXX}",
+                         conf)
+      end
+
+      def link_command(ldflags, *opts)
+        conf = link_config(ldflags, *opts)
+        RbConfig::expand(TRY_LINK_CXX.dup, conf)
+      end
+    end
+  end
+end
+
 # The cpp_command is not overwritten in the experimental mkmf C++ support.
 # See https://bugs.ruby-lang.org/issues/17578
 MakeMakefile['C++'].module_eval do
