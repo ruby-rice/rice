b9ddac7bfbd2dd392ae596ab0a830388f9fb07c6
Charlie Savage <cfis@savagexi.com>
2023-04-15 23:56:00 -0700

    More header order changes and comments.

--- rice/rice.hpp
+++ rice/rice.hpp
@@ -1,14 +1,19 @@
 #ifndef Rice__hpp_
 #define Rice__hpp_
 
+// Traits
 #include "detail/ruby.hpp"
 #include "traits/rice_traits.hpp"
 #include "traits/function_traits.hpp"
 #include "traits/method_traits.hpp"
 #include "traits/attribute_traits.hpp"
+
+// Code for C++ to call Ruby
 #include "Exception_defn.hpp"
 #include "detail/Jump_Tag.hpp"
 #include "detail/RubyFunction.hpp"
+
+// Code for Ruby to call C++
 #include "detail/ExceptionHandler.hpp"
 #include "detail/Type.hpp"
 #include "detail/TypeRegistry.hpp"
@@ -17,7 +22,6 @@
 #include "detail/NativeRegistry.hpp"
 #include "detail/Registries.hpp"
 #include "detail/cpp_protect.hpp"
-#include "detail/default_allocation_func.hpp"
 #include "detail/Wrapper.hpp"
 #include "Return.hpp"
 #include "Arg.hpp"
@@ -29,10 +33,10 @@
 #include "detail/NativeAttribute.hpp"
 #include "detail/NativeFunction.hpp"
 #include "detail/NativeIterator.hpp"
-
-#include "ruby_mark.hpp"
-
+#include "global_function.hpp"
 #include "Address_Registration_Guard.hpp"
+
+// C++ classes for using the Ruby API
 #include "cpp_api/Object.hpp"
 #include "cpp_api/Builtin_Object.hpp"
 #include "cpp_api/String.hpp"
@@ -43,8 +47,9 @@
 #include "cpp_api/Class.hpp"
 #include "cpp_api/Struct.hpp"
 
-#include "global_function.hpp"
-
+// Code involed in creating custom DataTypes (ie, Ruby classes that wrap C++ classes)
+#include "ruby_mark.hpp"
+#include "detail/default_allocation_func.hpp"
 #include "Director.hpp"
 #include "Data_Type.hpp"
 #include "detail/default_allocation_func.ipp"
