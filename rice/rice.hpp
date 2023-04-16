db9ff40ee2f259e81c5967c8dcb71f3e5ea3c074
Charlie Savage <cfis@savagexi.com>
2023-04-15 23:51:02 -0700

    Rearrange includes a little bit.

--- rice/rice.hpp
+++ rice/rice.hpp
@@ -24,36 +24,32 @@
 #include "detail/MethodInfo.hpp"
 #include "detail/from_ruby.hpp"
 #include "detail/to_ruby.hpp"
-#include "Exception.ipp"
 #include "Identifier.hpp"
+#include "Exception.ipp"
 #include "detail/NativeAttribute.hpp"
 #include "detail/NativeFunction.hpp"
 #include "detail/NativeIterator.hpp"
 
 #include "ruby_mark.hpp"
 
+#include "Address_Registration_Guard.hpp"
 #include "cpp_api/Object.hpp"
 #include "cpp_api/Builtin_Object.hpp"
 #include "cpp_api/String.hpp"
 #include "cpp_api/Array.hpp"
 #include "cpp_api/Hash.hpp"
 #include "cpp_api/Symbol.hpp"
-
-#include "Address_Registration_Guard.hpp"
 #include "cpp_api/Module.hpp"
-#include "global_function.hpp"
-
 #include "cpp_api/Class.hpp"
 #include "cpp_api/Struct.hpp"
 
+#include "global_function.hpp"
+
 #include "Director.hpp"
 #include "Data_Type.hpp"
+#include "detail/default_allocation_func.ipp"
 #include "Constructor.hpp"
 #include "Data_Object.hpp"
-
-// Dependent on Data_Object due to the way method metadata is stored in the Ruby class
-#include "detail/default_allocation_func.ipp"
-
 #include "Enum.hpp"
 
 // Dependent on Module, Class, Array and String
