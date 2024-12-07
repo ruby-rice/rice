d5b351b349ec5856fd3bc304f0ac8479733a2640
cfis <cfis@savagexi.com>
2024-12-07 11:22:37 -0800

    Add template tests.

--- test/test_Template.cpp
+++ test/test_Template.cpp
@@ -44,20 +44,21 @@ namespace
   };
 }
 
-template<typename T>
-void builder_function(Data_Type<T>& klass)
+template<typename Data_Type_T, typename T>
+void MyVector_builder(Data_Type_T& klass)
 {
-  klass.define_constructor(Constructor<T>())
-    .define_method("add", &T::add)
-    .define_method("size", &T::size)
-    .define_attr("empty", &T::empty, Rice::AttrAccess::Read);
+  klass.define_constructor(Constructor<MyVector<T>>())
+    .define_method("add", &MyVector<T>::add)
+    .define_method("size", &MyVector<T>::size)
+    .define_attr("empty", &MyVector<T>::empty, Rice::AttrAccess::Read);
 }
 
 TESTCASE(my_vector)
 {
-  Class C1 = define_class_template<MyVector, int>("MyVecInt", builder_function);
+  Class C1 = define_class<MyVector<int>>("MyVecInt").
+             define(&MyVector_builder<Data_Type<MyVector<int>>, int>);
+
   Object o1 = C1.create();
-  
   Object result1 = o1.instance_eval("empty");
   ASSERT_EQUAL(Qtrue, result1.value());
 
@@ -71,7 +72,9 @@ TESTCASE(my_vector)
   result1 = o1.instance_eval("size");
   ASSERT_EQUAL(1, detail::From_Ruby<int>().convert(result1.value()));
 
-  Class C2 = define_class_template<MyVector, std::string>("MyVecString", &builder_function);
+  Class C2 = define_class<MyVector< std::string>>("MyVecInt").
+             define(&MyVector_builder<Data_Type<MyVector<std::string>>, std::string>);
+
   Object o2 = C2.create();
   Object result2 = o2.instance_eval("empty");
   ASSERT_EQUAL(Qtrue, result2.value());
@@ -87,74 +90,37 @@ TESTCASE(my_vector)
   ASSERT_EQUAL(1, detail::From_Ruby<int>().convert(result2.value()));
 }
 
-TESTCASE(my_vector_lambda)
-{
-  auto builder_lambda = [](auto& klass)
-    {
-      using T = std::remove_reference_t<decltype(klass)>::type;
-      klass.define_constructor(Constructor<T>())
-        .define_method("add", &T::add)
-        .define_method("size", &T::size)
-        .define_attr("empty", &T::empty, Rice::AttrAccess::Read);
-    };
-
-  Class C = define_class_template<MyVector, float>("MyVecFloat", builder_lambda);
-  Object o = C.create();
-
-  Object result = o.instance_eval("empty");
-  ASSERT_EQUAL(Qtrue, result.value());
-
-  result = o.instance_eval("size");
-  ASSERT_EQUAL(0, detail::From_Ruby<int>().convert(result.value()));
-
-  o.call("add", 5.0);
-  result = o.instance_eval("empty");
-  ASSERT_EQUAL(Qfalse, o.instance_eval("empty").value());
-
-  result = o.instance_eval("size");
-  ASSERT_EQUAL(1, detail::From_Ruby<int>().convert(result.value()));
-}
-
 namespace
 {
-  template<typename T, int x, int y>
+  template<typename T, int Rows, int Columns>
   class Matrix
   {
-    Matrix(T something)
+  public:
+    Matrix()
     {
     }
 
-    Matrix<T, x, y> col(int i) const
+    int cols() const
     {
-      return Matrix<T, x, y>();
+      return Columns;
     }
   };
 }
 
+template<typename Data_Type_T, typename T, int Rows, int Columns>
+void Matrix_builder(Data_Type_T& klass)
+{
+  klass.define_constructor(Constructor<Matrix<T, Rows, Columns>>())
+    .define_method("cols", &Matrix<T, Rows, Columns>::cols);
+}
+
 TESTCASE(matrix)
 {
-  auto builder_lambda = [](auto& klass)
-    {
-      using T = std::remove_reference_t<decltype(klass)>::type;
-      klass.define_constructor(Constructor<T>())
-        .define_method("add", &T::add)
-        .define_method("size", &T::size)
-        .define_attr("empty", &T::empty, Rice::AttrAccess::Read);
-    };
+  Class C = define_class<Matrix<float, 5, 4>>("Matrixf54").
+    define(&Matrix_builder<Data_Type<Matrix<float, 5, 4>>, float, 5, 4>);
 
-  Class C = define_class_template<MyVector, float>("MyVecFloat", builder_lambda);
   Object o = C.create();
 
-  Object result = o.instance_eval("empty");
-  ASSERT_EQUAL(Qtrue, result.value());
-
-  result = o.instance_eval("size");
-  ASSERT_EQUAL(0, detail::From_Ruby<int>().convert(result.value()));
-
-  o.call("add", 5.0);
-  result = o.instance_eval("empty");
-  ASSERT_EQUAL(Qfalse, o.instance_eval("empty").value());
-
-  result = o.instance_eval("size");
-  ASSERT_EQUAL(1, detail::From_Ruby<int>().convert(result.value()));
+  Object result = o.instance_eval("cols");
+  ASSERT_EQUAL(4, detail::From_Ruby<int>().convert(result.value()));
 }
