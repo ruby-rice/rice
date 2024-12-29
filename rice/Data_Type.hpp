ruby-rice/rice

commit dfc50bf703cff35951ce264fe1fafd6c94a57aa6

Update std::exception to inherit from rb_eStandardError so custom exception classes can be throw by Ruby. This required adding a new override for define_class_under.

rice/Data_Type.hpp
@@ -152,11 +152,14 @@ namespace Rice
     template <typename Base_T = void>
     static Data_Type<T> bind(const Module& klass);
 
-    template<typename T_, typename Base_T_>
-    friend Rice::Data_Type<T_> define_class_under(Object module, char const * name);
+    template<typename T>
+    friend Rice::Data_Type<T> define_class_under(Object module, char const* name, Class superKlass);
 
-    template<typename T_, typename Base_T_>
-    friend Rice::Data_Type<T_> define_class(char const * name);
+    template<typename T, typename Base_T>
+    friend Rice::Data_Type<T> define_class_under(Object module, char const * name);
+
+    template<typename T, typename Base_T>
+    friend Rice::Data_Type<T> define_class(char const * name);
 
     template<bool IsMethod, typename Function_T>
     void wrap_native_call(VALUE klass, std::string name, Function_T&& function, MethodInfo* methodInfo);
@@ -185,6 +188,20 @@ namespace Rice
    */
   template<typename T, typename Base_T = void>
   Data_Type<T> define_class_under(Object module, char const* name);
+
+  //! Define a new data class in the namespace given by module.
+  /*! This override allows you to specify a Ruby class as the base class versus a 
+   *  wrapped C++ class. This functionality is rarely needed - but is essential for
+   *  creating new custom Exception classes where the Ruby superclass should be
+   *  rb_eStandard
+   *  \param T the C++ type of the wrapped class.
+   *  \param module the Module in which to define the class.
+   *  \param name the name of the new class.
+   *  \param superKlass the Ruby super class.
+   *  \return the new class.
+   */
+  template<typename T>
+  Data_Type<T> define_class_under(Object module, char const* name, Class superKlass);
 
   //! Define a new data class in the default namespace.
   /*! By default the class will inherit from Ruby's rb_cObject. This

rice/Data_Type.ipp
@@ -203,6 +203,26 @@ namespace Rice
     }
   }
 
+  template<typename T>
+  Rice::Data_Type<T> define_class_under(Object module, char const* name, Class superKlass)
+  {
+    // Is the class already defined?
+    if (detail::Registries::instance.types.isDefined<T>())
+    {
+      Data_Type<T> result = Data_Type<T>();
+      // If this redefinition is a different name then create a new constant
+      if (result.name().c_str() != name)
+      {
+        detail::protect(rb_define_const, module, name, result.klass());
+      }
+      return Data_Type<T>();
+    }
+
+    Class c = define_class_under(module, name, superKlass);
+    c.undef_creation_funcs();
+    return Data_Type<T>::template bind(c);
+  }
+
   template<typename T, typename Base_T>
   inline Data_Type<T> define_class_under(Object module, char const* name)
   {

rice/stl/exception.hpp
@@ -1,6 +1,11 @@
 #ifndef Rice__stl__exception__hpp_
 #define Rice__stl__exception__hpp_
 
+namespace Rice::stl
+{
+  extern Class rb_cStlException;
+}
+
 #include "exception.ipp"
 
 #endif // Rice__stl__exception__hpp_

rice/stl/exception.ipp
@@ -6,15 +6,15 @@ namespace Rice
 {
   namespace stl
   {
-    inline Data_Type<std::exception> define_stl_exception()
+    inline Class rb_cStlException;
+
+    inline void define_stl_exception()
     {
       Module rb_mRice = define_module("Rice");
-      Module rb_mmap = define_module_under(rb_mRice, "Std");
-      Data_Type<std::exception> rb_cStlException = define_class_under<std::exception>(rb_mmap, "Exception");
-      rb_cStlException.
-         define_constructor(Constructor<std::exception>()).
-         define_method("what", &std::exception::what);
-      return rb_cStlException;
+      Module rb_mStd = define_module_under(rb_mRice, "Std");
+      rb_cStlException = define_class_under<std::exception>(rb_mStd, "Exception", rb_eStandardError).
+                         define_constructor(Constructor<std::exception>()).
+                         define_method("message", &std::exception::what);
     }
   }
 }

commit bcd1bcc9090d95500ff8d347e9590c11cca4e983

Fix compile issue due to shadowed template name. This was broken in dfc50bf703cff35951ce264fe1fafd6c94a57aa6.

rice/Data_Type.hpp
@@ -152,14 +152,14 @@ namespace Rice
     template <typename Base_T = void>
     static Data_Type<T> bind(const Module& klass);
 
-    template<typename T>
-    friend Rice::Data_Type<T> define_class_under(Object module, char const* name, Class superKlass);
+    template<typename T_>
+    friend Rice::Data_Type<T_> define_class_under(Object module, char const* name, Class superKlass);
 
-    template<typename T, typename Base_T>
-    friend Rice::Data_Type<T> define_class_under(Object module, char const * name);
+    template<typename T_, typename Base_T>
+    friend Rice::Data_Type<T_> define_class_under(Object module, char const * name);
 
-    template<typename T, typename Base_T>
-    friend Rice::Data_Type<T> define_class(char const * name);
+    template<typename T_, typename Base_T>
+    friend Rice::Data_Type<T_> define_class(char const * name);
 
     template<bool IsMethod, typename Function_T>
     void wrap_native_call(VALUE klass, std::string name, Function_T&& function, MethodInfo* methodInfo);