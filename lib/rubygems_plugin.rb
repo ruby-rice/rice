cc765c510c73f4c27881d3f72b3a3eeeeecf8da1
cfis <cfis@savagexi.com>
2025-05-26 00:42:50 -0700

    Make sure installer.package is defined.

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,7 +1,7 @@
 # Create RubyGems hook so we can replace the default CMakeBuilder with our own version
 Gem.pre_install do |installer|
-  extensions = installer.package.spec.extensions
-  if extensions.grep(/CMakeLists/)
+  extensions = installer.package&.spec&.extensions
+  if extensions && extensions.grep(/CMakeLists/)
     require_relative 'rubygems/builder'
     require_relative 'rubygems/cmake_builder'
   end
