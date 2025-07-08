0716485062fee9e7236e48a5285d3e81e1fa6a11
cfis <cfis@savagexi.com>
2025-07-07 23:16:51 -0700

    Remove unused method.

--- rice/detail/Native.hpp
+++ rice/detail/Native.hpp
@@ -22,8 +22,6 @@ namespace Rice::detail
     static VALUE resolve(int argc, VALUE* argv, VALUE self);
   public:
     virtual ~Native() = default;
-    VALUE call(int argc, VALUE* argv, VALUE self);
-
     virtual Resolved matches(size_t argc, const VALUE* argv, VALUE self) = 0;
     virtual VALUE operator()(size_t argc, const VALUE* argv, VALUE self) = 0;
     virtual std::string toString() = 0;
