044a24dbb3363be0bcf7337d0fd79ecf9699803f
cfis <cfis@savagexi.com>
2025-11-27 01:03:24 -0800

    Updated docs

--- doc/conf.py
+++ doc/conf.py
@@ -27,8 +27,11 @@ author = 'Paul Brannon, Jason Roelofs, Charlie Savage'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['myst_parser']
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
