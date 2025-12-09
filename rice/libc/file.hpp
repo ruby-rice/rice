80ff4928b8157caf8fbe60cb9d99d18bd36ade4f
cfis <cfis@savagexi.com>
2025-12-08 21:48:08 -0800

    Fix misspelled namespace.

--- rice/libc/file.hpp
+++ rice/libc/file.hpp
@@ -1,11 +1,11 @@
-#ifndef Rice__libc__file__hpp_
-#define Rice__libc__file__hpp_
+#ifndef Rice__Libc__file__hpp_
+#define Rice__Libc__file__hpp_
 
-namespace Rice::libc
+namespace Rice::Libc
 {
   extern Class rb_cLibcFile;
 }
 
 #include "file.ipp"
 
-#endif // Rice__libc__file__hpp_
+#endif // Rice__Libc__file__hpp_
