fdc287f26d4e84caef9d2f5871261b252dd76d6a
cfis <cfis@savagexi.com>
2025-12-08 22:08:00 -0800

    RubyGems 4 has updated CMake builder.

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,9 +1,12 @@
-# Create RubyGems hook so we can replace the default CMakeBuilder with our own version
-Gem.pre_install do |installer|
-  extensions = installer.package&.spec&.extensions
-  if extensions && extensions.grep(/CMakeLists/)
-    require_relative 'rubygems/builder'
-    require_relative 'rubygems/cmake_builder'
-  end
-  true
+# For older versions of RubyGems replace the default CMakeBuilder with a
+# more modern one
+if Gem::Version.new(Gem::VERSION) < Gem::Version.new('4.0')
+	Gem.pre_install do |installer|
+		extensions = installer.package&.spec&.extensions
+		if extensions && extensions.grep(/CMakeLists/)
+			require_relative 'rubygems/builder'
+			require_relative 'rubygems/cmake_builder'
+		end
+		true
+	end
 end
