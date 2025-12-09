578c2f6ad457c551fc4fbedff34f4b3c56ab42cb
cfis <cfis@savagexi.com>
2025-12-08 22:08:57 -0800

    A better check for the current RubyGem version.

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,6 +1,6 @@
 # For older versions of RubyGems replace the default CMakeBuilder with a
 # more modern one
-if Gem::Version.new(Gem::VERSION) < Gem::Version.new('4.0')
+if Gem.rubygems_version < Gem::Version.new('4.0')
 	Gem.pre_install do |installer|
 		extensions = installer.package&.spec&.extensions
 		if extensions && extensions.grep(/CMakeLists/)
