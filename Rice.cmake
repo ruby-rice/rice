cec9750a407787f7bcf6ab75d154df4844130bfb
cfis <cfis@savagexi.com>
2026-01-21 00:57:33 -0800

    Make Rice target more robust, allow it to be included multiple times without breaking.

--- Rice.cmake
+++ Rice.cmake
@@ -1,11 +1,18 @@
-add_library(Rice INTERFACE)
-add_library(Rice::Rice ALIAS Rice)
+include_guard(GLOBAL)
 
-target_include_directories(Rice INTERFACE
-  $<BUILD_INTERFACE:$<$<CONFIG:Debug>:${CMAKE_CURRENT_SOURCE_DIR}>>
-  $<BUILD_INTERFACE:$<$<CONFIG:Release>:${CMAKE_CURRENT_SOURCE_DIR}/include>>
-  $<INSTALL_INTERFACE:include>
-)
+if(NOT TARGET Rice)
+  add_library(Rice INTERFACE)
 
-# Need C++17 or higher
-target_compile_features(Rice INTERFACE cxx_std_17)
+  target_include_directories(Rice INTERFACE
+    $<BUILD_INTERFACE:$<$<CONFIG:Debug>:${CMAKE_CURRENT_SOURCE_DIR}>>
+    $<BUILD_INTERFACE:$<$<CONFIG:Release>:${CMAKE_CURRENT_SOURCE_DIR}/include>>
+    $<INSTALL_INTERFACE:include>
+  )
+
+  # Need C++17 or higher
+  target_compile_features(Rice INTERFACE cxx_std_17)
+endif()
+
+if(NOT TARGET Rice::Rice)
+  add_library(Rice::Rice ALIAS Rice)
+endif()
